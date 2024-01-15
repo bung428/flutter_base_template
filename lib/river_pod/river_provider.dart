93ceb52fe5231a2912f1b106aa0c214d0f361c6c
Byounggoo Kim <bgkim@seerslab.com>
2024-01-15 11:19:24 +0900

    add rxDart

--- lib/river_pod/river_provider.dart
+++ lib/river_pod/river_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_base_template/river_pod/river_notifier.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -40,6 +41,5 @@ class RiverProviderState<T extends RiverNotifier<G>, G>
     return const SizedBox.shrink();
   }
 
-  Widget _buildProviderWidget(BuildContext context, Widget widget)
-  => widget;
+  Widget _buildProviderWidget(BuildContext context, Widget widget) => widget;
 }
